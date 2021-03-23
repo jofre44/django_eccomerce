314a9b441e406aa405c6204a2fdd24e464ec44bd
Isabel-Cristina <jofre44@gmail.com>
2021-03-23 17:17:37 +0100

    Making first models and views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView
+from .models import Item, OrderItem, Order
 
-# Create your views here.
+
+def products(request):
+    conext = {
+        'items': Item.objects.all()
+    }
+    return render(request, "products.html", context)
+
+
+def checkout(request):
+    return render(request, "checkout.html")
+
+
+class HomeView(ListView):
+    model = Item
+    template_name = "home.html"
+
+
+class IitemDetailView(DetailView):
+    model = Item
+    template_name = 'product.html'
+
+
+def add_to_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_item = OrderItem.objects.create(item=item)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item_slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+    else:
+        order = Order.objects.create(user=request.user)
+        order.items.add(order_item)
+    return redirect("core:product", kwargs={
+        'slug': slug
+    })
