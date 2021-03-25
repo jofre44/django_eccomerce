4e4f4ae646a1b47863b52d40e21c95841fea90f4
Isabel-Cristina <jofre44@gmail.com>
2021-03-25 17:22:18 +0100

    New pages for cart, ordersummary and login/logout/signup

--- core/views.py
+++ core/views.py
@@ -1,5 +1,10 @@
+from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
+from django.utils import timezone
 from .models import Item, OrderItem, Order
 
 
@@ -16,7 +21,21 @@ def checkout(request):
 
 class HomeView(ListView):
     model = Item
+    paginated_by = 10
     template_name = "home.html"
+
+
+class OrderSummaryView(LoginRequiredMixin, View):
+    def get(self, *args, **kwargs):
+        try:
+            order = Order.objects.get(user=self.request.user, ordered=False)
+            context = {
+                'object': order
+            }
+            return render(self.request, 'order_summary.html', context)
+        except ObjectDoesNotExist:
+            messages.error(self.request, "You do no haver an active order")
+            return redirect("/")
 
 
 class IitemDetailView(DetailView):
@@ -24,18 +43,47 @@ class IitemDetailView(DetailView):
     template_name = 'product.html'
 
 
+@login_required
 def add_to_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
-    order_item = OrderItem.objects.create(item=item)
+    order_item, created = OrderItem.objects.get_or_create(
+        item=item, user=request.user, ordered=False)
     order_qs = Order.objects.filter(user=request.user, ordered=False)
     if order_qs.exists():
         order = order_qs[0]
         if order.items.filter(item_slug=item.slug).exists():
             order_item.quantity += 1
             order_item.save()
+            messages.info(request, "This item quantity was updated.")
+            return redirect("core:product", slug=slug)
+        else:
+            messages.info(request, "This item was added to your cart.")
+            order.items.add(order_item)
+            return redirect("core:product", slug=slug)
     else:
-        order = Order.objects.create(user=request.user)
+        order_date = timezone.now()
+        order = Order.objects.create(
+            user=request.user, ordered_date=order_date)
         order.items.add(order_item)
-    return redirect("core:product", kwargs={
-        'slug': slug
-    })
+        messages.info("This item was added to your cart.")
+        return redirect("core:product", slug=slug)
+
+
+@login_required
+def remove_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item_slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item, user=requet.user, ordered=False)[0]
+            order.items.remove(order_item)
+            messages.info("This item was removed from your cart.")
+            return redirect("core:product", slug=slug)
+        else:
+            messages.info("This item was not in your cart.")
+            return redirect("core:product", slug=slug)
+    else:
+        messages.info("You do not have and active order.")
+        return redirect("core:product", slug=slug)
--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_forms',
     'core'
 ]
 
@@ -88,3 +89,8 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+
+# CRISOY FORMS
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
